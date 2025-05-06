2d1571b33e72672d8658efb2f9c604366eee49cc
Watcharaphong <65011212077@msu.ac.th>
2025-05-06 19:53:56 +0700

    fix date

--- api/dog.ts
+++ api/dog.ts
@@ -6,24 +6,30 @@ import { DogPost } from "../model/dogPost";
 export const router = express.Router();
 
 router.post("/", (req, res) => {
-    let dog: DogPost = req.body;
-    let sql = "INSERT INTO dog (user_email, name, breed, gender, color, defect, birthday, congentialDisease, sterilization, hair, image) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
-    sql = mysql.format(sql, [
-        dog.user_email,
-        dog.name,
-        dog.breed,
-        dog.gender,
-        dog.color,
-        dog.defect,
-        dog.birthday,
-        dog.congentialDisease,
-        dog.sterilization,
-        dog.hair,
-        dog.image
-    ])
+  let dog: DogPost = req.body;
+  
+  const [day, month, year] = dog.birthday.split("-");
+  const formattedDate = `${year}-${month.padStart(2, "0")}-${day.padStart(2,"0")}`;
+  console.log(formattedDate);
 
-    conn.query(sql, (err, result) => {
-        if (err) throw err;
-        res.status(201).json({ insertId: result.insertId  });
-    })
-})
+  let sql =
+    "INSERT INTO dog (user_email, name, breed, gender, color, defect, birthday, congentialDisease, sterilization, hair, image) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+  sql = mysql.format(sql, [
+    dog.user_email,
+    dog.name,
+    dog.breed,
+    dog.gender,
+    dog.color,
+    dog.defect,
+    formattedDate,
+    dog.congentialDisease,
+    dog.sterilization,
+    dog.hair,
+    dog.image,
+  ]);
+
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(201).json({ insertId: result.insertId });
+  });
+});

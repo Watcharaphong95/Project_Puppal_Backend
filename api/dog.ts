aa982d88d789ca332be8433ea2d57c53d0412ba8
Watcharaphong <65011212077@msu.ac.th>
2025-05-08 15:22:31 +0700

    add dog get by email

--- api/dog.ts
+++ api/dog.ts
@@ -5,6 +5,18 @@ import { DogPost } from "../model/dogPost";
 
 export const router = express.Router();
 
+router.get("/:email", (req, res) => {
+  let email = req.params.email;
+    let sql = "SELECT * FROM dog WHERE user_email = ?";
+    sql = mysql.format(sql, [
+      email
+    ])
+    conn.query(sql, (err, result) => {
+        if (err) throw err;
+        res.status(200).json(result);
+    })
+})
+
 router.post("/", (req, res) => {
   let dog: DogPost = req.body;
   

2891ff65c3bfd844c4d1c59484daeb96933fbab1
Watcharaphong <65011212077@msu.ac.th>
2025-06-03 12:29:52 +0700

    add injectionRecord get all by email user

--- api/dogInjectionRecord.ts
+++ api/dogInjectionRecord.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { conn } from "../dbconnect";
 import mysql from "mysql";
 import { InjectionRecordPost } from "../model/dogInjectionRecordPost";
+import e from "express";
 
 export const router = express.Router();
 
@@ -10,6 +11,19 @@ router.get("/:id", (req, res) => {
     let sql = "SELECT * FROM injectionRecord WHERE dog_id = ?"
     sql = mysql.format(sql, [
       id
+    ])
+    conn.query(sql, (err, result) => {
+        if (err) throw err;
+
+        res.status(200).json(result);
+      });
+})
+
+router.get("/all/:email", (req, res) => {
+    let email = req.params.email;
+    let sql = "SELECT injectionRecord.* FROM injectionRecord JOIN dog ON injectionRecord.dog_id = dog.dogId WHERE dog.user_email = ?"
+    sql = mysql.format(sql, [
+      email
     ])
     conn.query(sql, (err, result) => {
         if (err) throw err;

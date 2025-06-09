70adefd8d87613449c66b5af6317d5973be48e6c
Phuwapon999 <65011212193@msu.ac.th>
2025-06-10 00:57:25 +0700

    clinic

--- api/clinic/special.ts
+++ api/clinic/special.ts
@@ -1,6 +1,7 @@
 import { conn } from "../../dbconnect";
 import express from "express";
 import mysql from "mysql";
+import { SpecialPost } from "../../model/specialPost";
 
 export const router = express.Router();
 
@@ -13,12 +14,44 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
-  let {name} = req.body;
-  let sql = "INSERT INTO special (name) VALUES (?)";
-  sql = mysql.format(sql, [name]);
+  let specialArray: SpecialPost[] = req.body; // รับ array
+  specialArray.forEach((special) => {
+    let sql = "INSERT INTO special (name) VALUES (?)";
+    sql = mysql.format(sql, [special.name]);
+    conn.query(sql, (err, result) => {
+      if (err) throw err;
+    });
+  });
+  res.status(201).json({ message: "insert success" });
 
-  conn.query(sql, (err, result) => {
-    if (err) throw err;
-    res.status(201).json({ message: "insert success" });
+});
+
+// router.get("/search/:name", (req, res) => {
+//   const name = req.params.name; // รับจาก path param
+
+//   let sql = "SELECT * FROM special WHERE name = ?";
+//   sql = mysql.format(sql, [name]);
+
+//   conn.query(sql, (err, result) => {
+//     if (err) {
+//       return res.status(500).json({ message: "Database error", error: err });
+//     }
+//     res.status(200).json(result);
+//   });
+// });
+
+router.get("/search", (req, res) => {
+  const name = req.query.name;
+
+  let sql = "SELECT special_id FROM special WHERE name = ?";
+  let formattedSql = mysql.format(sql, [name]);
+
+  conn.query(formattedSql, (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Database error", error: err });
+    }
+    res.status(200).json(result);
   });
 });
+
+

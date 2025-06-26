faf36b44341c48f2c3ed3b505118bf2347e44295
Watcharaphong <65011212077@msu.ac.th>
2025-06-26 20:11:39 +0700

    add clinicSlotTime

--- api/reserve.ts
+++ api/reserve.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { conn } from "../dbconnect";
 import mysql from "mysql";
+import { ClinicSlotReq } from "../model/clinicSlotReq";
+import { json } from "body-parser";
 
 export const router = express.Router();
 
@@ -11,3 +13,19 @@ router.get("/", (req, res) => {
     res.status(200).json(result);
   });
 });
+
+router.post("/addRequest", (req, res) => {
+  let reserve: ClinicSlotReq = req.body;
+  let sql = "INSERT INTO reserve (general_email, clinic_email, dog_dogId, date, typeVaccine) VALUES (?,?,?,?,?)";
+  sql = mysql.format(sql, [
+    reserve.general_email,
+    reserve.clinic_email,
+    reserve.dog_dogId,
+    reserve.date,
+    reserve.typeVaccine,
+  ])
+  conn.query(sql, (err, result) => {
+    if(err) throw err;
+    res.status(201).json({ message: "insert complete"})
+  })
+})
